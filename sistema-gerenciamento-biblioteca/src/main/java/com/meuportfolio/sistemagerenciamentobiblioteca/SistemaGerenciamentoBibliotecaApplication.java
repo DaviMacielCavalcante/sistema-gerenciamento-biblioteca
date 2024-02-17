20636d5d17944f61eb1aa4db684b37829cc1a608
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2024-02-17 20:35:09 -0300

    criação do projeto

---
+++ sistema-gerenciamento-biblioteca/src/main/java/com/meuportfolio/sistemagerenciamentobiblioteca/SistemaGerenciamentoBibliotecaApplication.java
@@ -0,0 +1,13 @@
+package com.meuportfolio.sistemagerenciamentobiblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGerenciamentoBibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGerenciamentoBibliotecaApplication.class, args);
+	}
+
+}
---
+++ sistema-gerenciamento-biblioteca/src/test/java/com/meuportfolio/sistemagerenciamentobiblioteca/SistemaGerenciamentoBibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuportfolio.sistemagerenciamentobiblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGerenciamentoBibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
